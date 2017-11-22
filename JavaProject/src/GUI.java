2f4c81de6d3a803ef5a69337aca17ac51019d7eb
Markslaw <shaunalily9@gmail.com>
2017-11-22 16:56:36 +0000

    GUI

--- JavaProject/src/GUI.java
+++ JavaProject/src/GUI.java
@@ -13,26 +13,41 @@ import java.awt.event.ActionListener;
 
         // creating a JFrame called frame
         private JFrame frame = new JFrame("Blackjack");
-        JPanel panel = new JPanel();
+        //JPanel panel = new JPanel();
+        JLayeredPane panel = new JLayeredPane();
 
 
         public GUI()
 
         {
-            GridLayout layout = new GridLayout(0, 2);
+            JLabel label = new JLabel(new ImageIcon("Images//table.jpg"));
+
+           // GridLayout layout = new GridLayout(0, 2);
+            //frame.add(new ImageIcon("Images//table.jpg"));
+            frame.setLayout(null);
             frame.setLocation(350, 200);
             frame.setSize(1000, 800);
-            frame.setVisible(true);
+
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().setBackground(Color.green);
+          //  frame.getContentPane().setBackground(Color.blue);
+            frame.setVisible(true);
+           // frame.setLayout(layout);
+            //frame.getContentPane().setBackground(Color.green);
             //frame.add(this);
             frame.add(panel);
-            panel.add(hitbutton);
-            panel.add(staybutton);
-            panel.setSize(200,200);
+            hitbutton.setLocation(10,1);
+            hitbutton.setSize(100,100);
+            panel.add(label,1);
+            panel.add(hitbutton,2);
+            panel.add(staybutton,2);
+            panel.setSize(1000,800);
+            panel.setLocation(0,0);
+            panel.setBackground(Color.green);
 
-            Container contentPane = new Container();
-            contentPane.setLayout(layout);
+
+
+            //Container contentPane = new Container();
+            //contentPane.setLayout(layout);
             //contentPane.setBackground(Color.GREEN);
 
 
@@ -44,11 +59,6 @@ import java.awt.event.ActionListener;
 
             g.drawImage(table.getImage(), 0, 0, null);
         }
-
-
-
-
-
 
         private void FileMenu(){
             fileMenu = new JMenu("File");
